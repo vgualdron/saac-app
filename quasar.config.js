bb1d6827d42b0e5c6c524ed24bd10bba1e600a91
vgualdron <victor.gualdron.r@gmail.com>
2025-01-25 13:18:51 -0500

    as

--- quasar.config.js
+++ quasar.config.js
@@ -160,16 +160,14 @@ export default defineConfig((ctx) => {
         background_color: '#ffffff', // Fondo de la pantalla de carga
         theme_color: '#65a30d', // Cambia aquí el color de la barra superior
         icons: [
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-432x432.png',
-            sizes: '432x432',
-            type: 'image/png',
-          },
+          { src: 'icons/icon-72x72.png', sizes: '72x72', type: 'image/png' },
+          { src: 'icons/icon-96x96.png', sizes: '96x96', type: 'image/png' },
+          { src: 'icons/icon-128x128.png', sizes: '128x128', type: 'image/png' },
+          { src: 'icons/icon-144x144.png', sizes: '144x144', type: 'image/png' },
+          { src: 'icons/icon-152x152.png', sizes: '152x152', type: 'image/png' },
+          { src: 'icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-384x384.png', sizes: '384x384', type: 'image/png' },
+          { src: 'icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
     },
